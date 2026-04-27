f8fbfca26a6735aabd29c693dbd61d4039374005
lukerossi8 <lukerossi2026@gmail.com>
2026-04-27 16:37:56 -0400

    Update random bonded position setting
    Before, the random bonded position setting was erroneously always prescribing exactly three bonds. Now, it prescribes just as many bonds as can be formed if each agent bonds with one other agent — namely, half of the number of total agents in the simulation, rounded down.

--- Bioreactor Simulation Flows/cell_contact_sim_flowfield.m
+++ Bioreactor Simulation Flows/cell_contact_sim_flowfield.m
@@ -92,13 +92,13 @@ Bonded_pairs = zeros(n_agents, n_agents);
 
 % Set positions of agents — randomly, randomly with bonds, or manually
 
-% Random position setting
+% Position setting
 if isa(position, "string")
     if position == "random"
         x0 = l_wall + 0.35*(r_wall - l_wall) + 0.3*(r_wall - l_wall).*rand(n_agents, 1);
         y0 = floor + 0.05*(ceil - floor) + 0.15*(ceil - floor).*rand(n_agents, 1);
     elseif position == "random bonded" % Random bonded position setting
-        n_bonds = 3; % must be in [0, n_agents/2]
+        n_bonds = floor(n_agents/2);
         n_unbonded = n_agents - 2*n_bonds;
         x01 = l_wall + 0.1*(r_wall - l_wall) + 0.4*(r_wall - l_wall).*rand(n_bonds, 1);
         x02 = x01 + 14e-5;
